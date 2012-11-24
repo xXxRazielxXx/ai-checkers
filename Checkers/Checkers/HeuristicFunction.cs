59586b83920e34bdded71b3d8c719f5792020f39
sheirabh@gmail.com <sheirabh@gmail.com@7b8d6bbf-ea92-fb7e-7a8e-d56c0ade635f>
2012-11-24 14:32:42 +0000

    I've changed the following bla bla

--- Checkers/Checkers/HeuristicFunction.cs
+++ Checkers/Checkers/HeuristicFunction.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CheckersModel;
 
 namespace CheckersEngine
 {
     public class HeuristicFunction
     {
+        // calculates the pieces 'safeness' 
+        // assumption: the computer is the black player
+        public int HeuristicFunction(Board board)
+        {
+            int king_constant=10;
+            int soldier_constant=1;
+            int NumOfWhites = board.NumberOfWhitePieces;
+            int NumOfBlacks = board.NumberOfBlackPieces;
+            int NumWhiteKings = board.NumberOfWhiteKings;
+            int NumBlackKings = board.NumberOfBlcakKings;
+
+            int num_of_white_sold = (NumOfWhites - NumWhiteKings);
+            int num_of_black_sold = (NumOfBlacks - NumBlackKings);
+
+            return (((num_of_black_sold * soldier_constant) + (NumBlackKings* king_constant)) -((num_of_white_sold*soldier_constant)+(NumWhiteKings*king_constant));
+        }
     }
 }
--- Checkers/Checkers/MiniMax.cs
+++ Checkers/Checkers/MiniMax.cs
@@ -2,10 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using CheckersModel;
+using CheckersEngine;
+
 
 namespace CheckersEngine
 {
     class MiniMax
     {
+        int treeDepth = 100;
+        //player=1 means max player=0 means min
+        public int MinMax(Board board, int depth, bool player)
+        {
+
+            if (depth >= treeDepth)
+            {
+                HeuristicFunction obj = new HeuristicFunction();
+                return obj.HeuristicFunction(board);
+            }
+            else
+            {
+                int min = 10000;
+                int max = -10000;
+                foreach (Borad b in [NextGamePositions(board)])
+                {
+                    int res =MinMax( b (depth+1) !player);
+                    if( res > max) max=res;
+                    if (res < min) min= res;
+                       
+                }
+                             
+                if(player) return max;
+                else return min;               
+            }
+            
+        }
+
     }
 }
